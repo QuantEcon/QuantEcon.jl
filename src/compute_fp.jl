8ad5b7e4b16b61f4cb150260d9aaf56e2ba52769
Spencer Lyon <spencerlyon2@gmail.com>
2014-08-19 17:43:39 -0700

    STY: removed python style docstring

--- src/compute_fp.jl
+++ src/compute_fp.jl
@@ -20,19 +20,6 @@ http://quant-econ.net/dp_intro.html
     condition and k is the number of iterates. Provided that T is a
     contraction mapping or similar, T^k v will be an approximation to
     the fixed point.
-
-    Parameters
-    ==========
-    T : Function
-        representing the T mapping (operator)
-    v : Vector{S <: FloatingPoint}
-        Initial values for the mapping
-    error_tol : S <: FloatingPoint, optional(default=1e-3)
-        error tolerance for convergence
-    max_iter : Int, optional(default=50)
-        Maximum number of iterations before stopping
-    verbose : Bool, optional(default=true)
-        Whether or not to print a status update each iteration
 =#
 function compute_fixed_point(T::Function, v; err_tol=1e-3, max_iter=50,
                              verbose=true, print_skip=10)
