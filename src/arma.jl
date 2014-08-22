a0527fe57b77c6731539fab7ed526b606e7bb5f1
John Stachurski <john.stachurski@gmail.com>
2014-08-22 11:57:50 +1000

    Finished first pass of arma.jl

--- src/arma.jl
+++ src/arma.jl
@@ -12,6 +12,15 @@ References
 Simple port of the file quantecon.arma
 
 http://quant-econ.net/arma.html
+
+An example of usage is
+
+phi = 0.5
+theta = [0.0, -0.8]
+sigma = 1.0
+lp = ARMA(phi, theta, sigma)
+quad_plot(lp)
+
 =#
 
 import PyPlot.plt
@@ -41,6 +50,7 @@ function ARMA{T <: Real}(phi::Union(Vector{T}, T), theta::Union(Vector{T}, T), s
 end
 
 function spectral_density(arma::ARMA; res=1200, two_pi=true)
+    # Compute the spectral density associated with ARMA process arma
     wmax = two_pi ? 2pi : pi
     w = linspace(0, wmax, res)
     tf = TFFilter(reverse(arma.ma_poly), reverse(arma.ar_poly))
@@ -50,6 +60,8 @@ function spectral_density(arma::ARMA; res=1200, two_pi=true)
 end
 
 function autocovariance(arma::ARMA; num_autocov=16)
+    # Compute the autocovariance function associated with ARMA process arma
+    # Computation is via the spectral density and inverse FFT
     (w, spect) = spectral_density(arma)
     acov = real(Base.ifft(spect))
     # num_autocov should be <= len(acov) / 2
@@ -57,6 +69,7 @@ function autocovariance(arma::ARMA; num_autocov=16)
 end
 
 function impulse_response(arma::ARMA; impulse_length=30)
+    # Compute the impulse response function associated with ARMA process arma
     err_msg = "Impulse length must be greater than number of AR coefficients"
     @assert impulse_length >= arma.p err_msg
     # == Pad theta with zeros at the end == #
@@ -72,6 +85,18 @@ function impulse_response(arma::ARMA; impulse_length=30)
     return [psi_zero, psi[1:end-1]]
 end
 
+function simulation(arma::ARMA; ts_length=90, impulse_length=30)
+    # Simulate the ARMA process arma assuing Gaussian shocks
+    J = impulse_length 
+    T = ts_length
+    psi = impulse_response(arma, impulse_length=impulse_length)
+    epsilon = arma.sigma * randn(T + J)
+    X = Array(Float64, T)
+    for t=1:T
+        X[t] = dot(epsilon[t:J+t-1], psi)
+    end
+    return X
+end
 
 # == Plot functions == #
 
@@ -129,6 +154,24 @@ function plot_impulse_response(arma::ARMA; ax=None, show=true)
     end
 end
 
+function plot_simulation(arma::ARMA; ax=None, show=true)
+    X = simulation(arma)
+    n = length(X)
+    if show
+        fig, ax = plt.subplots()
+    end
+    ax[:set_title]("Sample path")
+    ax[:set_xlim](0.0, n)
+    ax[:set_xlabel]("time")
+    ax[:set_ylabel]("state space")
+    ax[:plot](0:(n-1), X, color="blue", lw=2, alpha=0.7)
+    if show
+        plt.show()
+    else
+        return ax
+    end
+end
+
 function quad_plot(arma::ARMA)
     (num_rows, num_cols) = (2, 2)
     fig, axes = plt.subplots(num_rows, num_cols, figsize=(12, 8))
@@ -136,21 +179,11 @@ function quad_plot(arma::ARMA)
     plot_functions = [plot_impulse_response,
                      plot_spectral_density,
                      plot_autocovariance,
-                     plot_impulse_response]
-                     #plot_simulation]
+                     plot_simulation]
     for (plot_func, ax) in zip(plot_functions, reshape(axes, 1, 4))
         plot_func(arma, ax=ax, show=false)
     end
     plt.show()
 end
 
-
-# == Testing == #
-
-phi = 0.5
-theta = [0.0, -0.8]
-sigma = 1.0
-lp = ARMA(phi, theta, sigma)
-#plot_spectral_density(lp)
-quad_plot(lp)
 
