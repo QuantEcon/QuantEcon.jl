199ed785bf373f5a59a22aac82a8e87af9da0790
Spencer Lyon <spencerlyon2@gmail.com>
2014-08-24 00:58:03 -0400

    QUAD: wrote all other "internal functions" from quad.py

--- src/quad.jl
+++ src/quad.jl
@@ -14,9 +14,7 @@ Miranda, Mario J, and Paul L Fackler. Applied Computational Economics
 and Finance, MIT Press, 2002.
 =#
 
-#=
-    Utilities
-=#
+## Utilities
 
 function fix!{T <: Real}(x::Array{T}, out::Array{T})
     for i=1:length(x)  # use linear indexing
@@ -27,7 +25,7 @@ end
 
 fix{T <: Real}(x::Array{T}) = fix!(x, similar(x, Int))
 
-fix{T <: Real}(x::T) = x >= 0 ? floor(x) : ceil(x)
+fix{T <: Real}(x::T) = int(x >= 0 ? floor(x) : ceil(x))
 
 ckron(A::Array, B::Array) = kron(A, B)
 ckron(arrays::Array...) = reduce(kron, arrays)
@@ -88,9 +86,7 @@ function make_multidim_func(one_d_func::Function, n, args...)
 end
 
 
-#=
-    1d Functions
-=#
+## 1d Functions
 
 function qnwlege(n::Int, a::Real, b::Real)
     maxit = 10000
@@ -142,12 +138,271 @@ function qnwlege(n::Int, a::Real, b::Real)
 end
 
 
+function qnwcheb(n::Int, a::Real, b::Real)
+    nodes = (b+a)/2 - (b-a)/2 .* cos(pi/n .* (0.5:(n-0.5)))
+    weights = ((b-a)/n) .* (cos(pi/n .* ([1:n]-0.5)*[0:2:n-1]')
+                            *[1, -2./([1:2:n-2].*[3:2:n])])
+    return nodes, weights
+end
+
+
+function qnwnorm(n::Int)
+    maxit = 100
+    pim4 = 1 / pi^(0.25)
+    m = fix((n + 1) / 2)
+    nodes = zeros(n)
+    weights = zeros(n)
+
+    for i=1:m
+        # Reasonable starting values for root finding
+        if i == 1
+            z = sqrt(2n+1) - 1.85575 * ((2n+1).^(-1/6))
+        elseif i == 2
+            z = z - 1.14 * (n.^0.426)./z
+        elseif i == 3
+            z = 1.86z + 0.86nodes[1]
+        elseif i == 4
+            z = 1.91z + 0.91nodes[2]
+        else
+            z = 2z + nodes[i-2]
+        end
+
+        # root finding iterations
+        it = 0
+        pp = 0.0  # initialize pp so it is available outside while
+        while it < maxit
+            it += 1
+            p1 = pim4
+            p2 = 0
+
+            for j=1:n
+                p3 = p2
+                p2 = p1
+                p1 = z .* sqrt(2/j) .*p2 - sqrt((j-1)/j).*p3
+            end
+
+            pp = sqrt(2n).*p2
+            z1 = z
+            z = z1 - p1./pp
+
+            if abs(z - z1) < 1e-14
+                break
+            end
+        end
+
+        if it >= maxit
+            error("Failed to converge in qnwnorm")
+        end
+
+        nodes[n+1-i] = z
+        nodes[i] = -z
+        weights[i] = 2 ./ (pp.*pp)
+        weights[n+1-i] = weights[i]
+    end
+
+    weights ./= sqrt(pi)
+    nodes *= sqrt(2)
+    return nodes, weights
+end
+
+
+function qnwsimp(n::Int, a::Real, b::Real)
+    if n<=1
+        error("In qnwsimp: n must be integer greater than one.")
+    end
+
+    if n % 2 ==0
+        warn("In qnwsimp: n must be odd integer - increasing by 1.")
+        n += 1
+    end
+
+    dx = (b - a) / (n - 1)
+    nodes = [a:dx:b]
+    weights = repeat([2.0, 4.0], outer=Int[(n + 1.0) / 2.0])
+    weights = weights[1:n]
+    weights[1] = 1
+    weights[end] = 1
+    weights = (dx / 3) * weights
+    return nodes, weights
+end
+
+
+function qnwtrap(n::Int, a::Real, b::Real)
+    if n < 1
+        error("n must be at least 1")
+    end
+
+    dx = (b - a) / (n - 1)
+    nodes = [a:dx:b]
+    weights = fill(dx, n)
+    weights[[1, n]] .*= 0.5
+    return nodes, weights
+end
+
+
+function qnwbeta(n::Int, a::Real, b::Real)
+    a -= 1
+    b -= 1
+    ab = a + b
+
+    maxit = 25
+
+    x = zeros(n)
+    w = zeros(n)
+
+    z::Float64 = 0.0
+
+    for i=1:n
+        if i == 1
+            an = a / n
+            bn = b / n
+            r1 = (1 + a) * (2.78 / (4 + n * n) + 0.768an / n)
+            r2 = 1 + 1.48 * an + 0.96bn + 0.452an*an + 0.83an*bn
+            z = 1 - r1 / r2
+
+        elseif i == 2
+            r1 = (4.1 + a) / ((1 + a) * (1 + 0.156a))
+            r2 = 1 + 0.06 * (n - 8) * (1 + 0.12a) / n
+            r3 = 1 + 0.012b * (1 + 0.25 * abs(a)) / n
+            z = z - (1 - z) * r1 * r2 * r3
+
+        elseif i == 3
+            r1 = (1.67 + 0.28a) / (1 + 0.37a)
+            r2 = 1 + 0.22 * (n - 8) / n
+            r3 = 1 + 8 * b / ((6.28 + b) * n * n)
+            z = z - (x[1] - z) * r1 * r2 * r3
+
+        elseif i == n - 1
+            r1 = (1 + 0.235b) / (0.766 + 0.119b)
+            r2 = 1 / (1 + 0.639 * (n - 4) / (1 + 0.71 * (n - 4)))
+            r3 = 1 / (1 + 20a / ((7.5+ a ) * n * n))
+            z = z + (z - x[n-3]) * r1 * r2 * r3
+
+        elseif i == n
+            r1 = (1 + 0.37b) / (1.67 + 0.28b)
+            r2 = 1 / (1 + 0.22 * (n - 8) / n)
+            r3 = 1 / (1 + 8 * a / ((6.28+ a ) * n * n))
+            z = z + (z - x[n-2]) * r1 * r2 * r3
+
+        else
+            z = 3 * x[i-1] - 3 * x[i-2] + x[i-3]
+        end
+
+        its = 1
+        temp = 0.0
+        pp, p2 = 0.0, 0.0
+        for its = 1:maxit
+            temp = 2 + ab
+            p1 = (a - b + temp * z) / 2
+            p2 = 1
+            for j=2:n
+              p3 = p2
+              p2 = p1
+              temp = 2 * j + ab
+              aa = 2 * j * (j + ab) * (temp - 2)
+              bb = (temp - 1) * (a * a - b * b + temp * (temp - 2) * z)
+              c = 2 * (j - 1 + a) * (j - 1 + b) * temp
+              p1 = (bb * p2 - c * p3) / aa
+            end
+            pp = (n * (a - b - temp * z) * p1 +
+                  2 * (n + a) * (n + b) * p2) / (temp * (1 - z * z))
+            z1 = z
+            z = z1 - p1 ./ pp
+            if abs(z - z1) < 3e-14 break end
+        end
+
+        if its >= maxit
+            error("Failure to converge in qnwbeta")
+        end
+
+        x[i] = z
+        w[i] = temp / (pp * p2)
+    end
+
+    x = (1 - x) ./ 2
+    w = w * exp(lgamma(a + n) +
+                lgamma(b + n) -
+                lgamma(n + 1) -
+                lgamma(n + ab + 1))
+    w = w / (2 * exp(lgamma(a + 1) +
+                     lgamma(b + 1) -
+                     lgamma(ab + 2)))
+
+    return x, w
+end
+
+
+function qnwgamma(n::Int, a::Real=1.0, b::Real=1.0)
+    a < 0 && error("shape parameter must be positive")
+    b < 0 && error("scale parameter must be positive")
+
+    a -= 1
+    maxit = 10
+    fact = -exp(lgamma(a+n)-lgamma(n)-lgamma(a+1))
+    nodes = zeros(n)
+    weights = zeros(n)
+
+    for i=1:n
+        # get starting values
+        if i==1
+            z = (1+a)*(3 + 0.92a)/(1 + 2.4n + 1.8a)
+        elseif i==2
+            z += (15 + 6.25a)./(1 + 0.9a + 2.5n)
+        else
+            j = i-2
+            z += ((1+2.55j)./(1.9j) + 1.26j*a./(1+3.5j)) * (z-nodes[j])./(1+0.3a)
+        end
+
+        # rootfinding iterations
+        it = 0
+        pp = 0.0
+        p2 = 0.0
+        for it = 1:maxit
+            p1 = 1.0
+            p2 = 0.0
+            for j=1:n
+                p3 = p2
+                p2 = p1
+                p1 = ((2j - 1 + a - z) * p2 - (j - 1 + a) * p3) ./ j
+            end
+            pp = (n*p1-(n+a)*p2)./z
+            z1 = z
+            z = z1-p1 ./ pp
+            if abs(z - z1) < 3e-14
+                break
+            end
+        end
+
+        if it >= maxit
+            error("failure to converge.")
+        end
+
+        nodes[i] = z
+        weights[i] = fact / (pp * n * p2)
+    end
+
+    return nodes .* b, weights
+end
+
+
+
+
+
+
+#= in multidim macro define functions for
+n::Real, a::Vector, b::Real
+n::Real, a::Vector, b::Vector
+n::Vector, a::Vector, b::Real
+
+ect so that all combinations are covered =#
+
+
+
+
 qnwlege(n, a, b) = make_multidim_func(qnwlege, n, a, b)
 
 
-#=
-    Doing the quadrature
-=#
+## Doing the quadrature
 
 function do_quad(f::Function, nodes::Array, weights::Vector, args...;
                  kwargs...)
