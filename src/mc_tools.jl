2e47578e97d6efc90a1bb3d9faef5f6b120f4206
zaccranko <zaccranko@gmail.com>
2014-11-26 14:16:14 +1100

    Add gth_solve to mc_tools.jl

--- src/mc_tools.jl
+++ src/mc_tools.jl
@@ -66,6 +66,49 @@ function lu_solve{T}(p::Matrix{T})
     x
 end
 
+gth_solve{T<:Integer}(A::Matrix{T}) = gth_solve(float64(A))
+
+function gth_solve{T<:Real}(A::AbstractMatrix{T})
+    if size(A, 1) != size(A, 2)
+        throw(ArgumentError("matrix must be square"))
+    end
+
+    A1 = copy(A)
+    n = size(A1, 1)
+    x = zeros(T, n)
+
+    # === Reduction === #
+    for k in 1:n-1
+        scale = sum(A1[k, k+1:n])
+        if scale <= 0
+            # There is one (and only one) recurrent class contained in
+            # {1, ..., k};
+            # compute the solution associated with that recurrent class.
+            n = k
+            break
+        end
+        for i in k+1:n
+            A1[i, k] /= scale
+        end
+        for j in k+1:n
+            for i in k+1:n
+                A1[i, j] += A1[i, k] * A1[k, j]
+            end
+        end
+    end
+
+    # === Backward substitution === #
+    x[n] = 1
+    for k in n-1:-1:1
+        for i in k+1:n
+            x[k] += x[i] * A1[i, k]
+        end
+    end
+
+    # === Normalization === #
+    x / sum(x)
+end
+
 # find the reducible subsets of a markov chain
 function irreducible_subsets(mc::MarkovChain)
     p = bool(mc.p)
