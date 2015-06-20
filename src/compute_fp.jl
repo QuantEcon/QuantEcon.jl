f210caea9db88c813fc0712d4d5f6ea4807f2451
Spencer Lyon <spencerlyon2@gmail.com>
2015-06-20 16:03:08 -0400

    ENH: added docstring to compte_fp.
    Also required Docile

--- src/compute_fp.jl
+++ src/compute_fp.jl
@@ -9,24 +9,50 @@ specified initial condition v.
 References
 ----------
 
-Simple port of the file quantecon.compute_fp
-
-http://quant-econ.net/dp_intro.html
+http://quant-econ.net/jl/dp_intro.html
 =#
 
 
-#=
-    Computes and returns T^k v, where T is an operator, v is an initial
-    condition and k is the number of iterates. Provided that T is a
-    contraction mapping or similar, T^k v will be an approximation to
-    the fixed point.
-=#
-function compute_fixed_point(T::Function, v; err_tol=1e-3, max_iter=50,
-                             verbose=true, print_skip=10)
+@doc doc"""
+Repeatedly apply a function to search for a fixed point
+
+Approximates `T^∞ v`, where `T` is an operator (function) and `v` is an initial
+guess for the fixed point. Will terminate either when `T^{k+1}(v) - T^k v <
+err_tol` or `max_iter` iterations has been exceeded.
+
+Provided that `T` is a contraction mapping or similar,  the return value will
+be an approximation to the fixed point of `T`.
+
+### Arguments
+
+* `T`: A function representing the operator `T`
+* `v::TV`: The initial condition. An object of type `TV`
+* `;err_tol(1e-3)`: Stopping tolerance for iterations
+* `;max_iter(50)`: Maximum number of iterations
+* `;verbose(true)`: Whether or not to print status updates to the user
+* `;print_skip(10)` : if `verbose` is true, how many iterations to apply between
+  print messages
+
+### Returns
+---
+
+* '::TV': The fixed point of the operator `T`. Has type `TV`
+
+### Example
+
+```julia
+using QuantEcon
+T(x, μ) = 4.0 * μ * x * (1.0 - x)
+x_star = compte_fixed_point(x->T(x, 0.3), 0.4)  # (4μ - 1)/(4μ)
+```
+
+""" ->
+function compute_fixed_point_old{TV}(T::Function, v::TV; err_tol=1e-3,
+                                     max_iter=50, verbose=true, print_skip=10)
     iterate = 0
     err = err_tol + 1
     while iterate < max_iter && err > err_tol
-        new_v = T(v)
+        new_v = T(v)::TV
         iterate += 1
         err = Base.maxabs(new_v - v)
         if verbose
