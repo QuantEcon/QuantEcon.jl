27e436a61373b932502f8362fbfbc543a425ccc3
Spencer Lyon <spencerlyon2@gmail.com>
2015-10-31 15:07:26 -0400

    Added basic tests for rouwenhorst method

--- test/test_markov_approx.jl
+++ test/test_markov_approx.jl
@@ -8,12 +8,13 @@ using FactCheck
 rough_kwargs = Dict(:atol => 1e-8, :rtol => 1e-8)
 
 # set up
+srand(42)
 ρ, σ_u = rand(2)
 μ = 0.0
 
 facts("Testing markov_approx.jl") do
-    for n=3:25, m=1:4
-        x, P = tauchen(n, ρ, σ_u, μ, m)
+    for n=3:25
+        x, P = rouwenhorst(n, ρ, σ_u, μ)
 
         @fact size(P, 1) --> size(P, 2)
         @fact ndims(x) --> 1
@@ -21,6 +22,17 @@ facts("Testing markov_approx.jl") do
         @fact sum(P, 2) --> roughly(ones(n, 1); rough_kwargs...)
         @fact all(P .>= 0.0) --> true
         @fact sum(x) --> roughly(0.0; rough_kwargs...)
+
+        for m=1:4
+            x, P = tauchen(n, ρ, σ_u, μ, m)
+
+            @fact size(P, 1) --> size(P, 2)
+            @fact ndims(x) --> 1
+            @fact ndims(P) --> 2
+            @fact sum(P, 2) --> roughly(ones(n, 1); rough_kwargs...)
+            @fact all(P .>= 0.0) --> true
+            @fact sum(x) --> roughly(0.0; rough_kwargs...)
+        end
     end
 end  # facts
 end  # module
