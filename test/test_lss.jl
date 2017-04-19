bfe36ce0a82e628d21ddd179faf448e94e62d1eb
Shunsuke Hori <hori0518@gmail.com>
2017-04-19 17:58:44 +0900

    fixing error of test

--- test/test_lss.jl
+++ test/test_lss.jl
@@ -82,7 +82,7 @@
 
         lss_psd = LSS(A, C, G, mu_0, Sigma_0)
 
-        @test rand(lss_psd) <: Array{Float64}
+        @test typeof(rand(lss_psd.dst)) == Array{Float64}
     end
 
 end  # @testset
