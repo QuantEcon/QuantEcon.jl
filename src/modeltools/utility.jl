8a5f3b7d284ef0200b7396311f6305faddb8ca9a
Chase <cc7768@gmail.com>
2017-10-20 17:43:09 -0400

    Change how to evaluate values less than 1e-10

--- src/modeltools/utility.jl
+++ src/modeltools/utility.jl
@@ -6,6 +6,16 @@ abstract type AbstractUtility end
 
 # Consumption utility
 
+"""
+Type used to evaluate log utility. Log utility takes the form
+
+u(c) = \log(c)
+
+Additionally, this code assumes that if c < 1e-10 then
+
+u(c) = log(1e-10) + 1e10*(c - 1e-10)
+
+"""
 struct LogUtility <: AbstractUtility
     ξ::Float64
 end
@@ -13,11 +23,19 @@ end
 LogUtility() = LogUtility(1.0)
 
 (u::LogUtility)(c::Float64) =
-    ifelse(c > 1e-10, u.ξ*log(c), -1e10)
+    ifelse(c > 1e-10, u.ξ*log(c), u.ξ*(log(1e-10) + 1e10*(c - 1e-10)))
 derivative(u::LogUtility, c::Float64) =
-    ifelse(c > 1e-10, u.ξ / c, 1e10)
+    ifelse(c > 1e-10, u.ξ / c, u.ξ*1e10)
 
+"""
+Type used to evaluate CRRA utility. CRRA utility takes the form
 
+u(c) = ξ c^(1 - γ) / (1 - γ)
+
+Additionally, this code assumes that if c < 1e-10 then
+
+u(c) = ξ (1e-10^(1 - γ) / (1 - γ) + 1e-10^(-γ) * (c - 1e-10))
+"""
 struct CRRAUtility <: AbstractUtility
     γ::Float64
     ξ::Float64
@@ -32,13 +50,25 @@ struct CRRAUtility <: AbstractUtility
 end
 
 (u::CRRAUtility)(c::Float64) =
-    ifelse(c > 1e-10, u.ξ * (c^(1.0 - u.γ) - 1.0) / (1.0 - u.γ), -1e10)
+    ifelse(c > 1e-10,
+           u.ξ * (c^(1.0 - u.γ) - 1.0) / (1.0 - u.γ),
+           u.ξ * ((1e-10^(1.0 - u.γ) - 1.0) / (1.0 - u.γ) + 1e-10^(-u.γ)*(c - 1e-10)))
 derivative(u::CRRAUtility, c::Float64) =
-    ifelse(c > 1e-10, u.ξ * c^(-u.γ), 1e10)
+    ifelse(c > 1e-10, u.ξ * c^(-u.γ), u.ξ*1e-10^(-u.γ))
 
 
 # Labor Utility
 
+"""
+Type used to evaluate constant Frisch elasticity (CFE) utility. CFE
+utility takes the form
+
+v(l) = ξ l^(1 + 1/ϕ) / (1 + 1/ϕ)
+
+Additionally, this code assumes that if l < 1e-10 then
+
+v(l) = ξ (1e-10^(1 + 1/ϕ) / (1 + 1/ϕ) - 1e-10^(1/ϕ) * (1e-10 - l))
+"""
 struct CFEUtility <: AbstractUtility
     ϕ::Float64
     ξ::Float64
@@ -53,11 +83,18 @@ struct CFEUtility <: AbstractUtility
 end
 
 (u::CFEUtility)(l::Float64) =
-    ifelse(l > 1e-10, -u.ξ * l^(1.0 + 1.0/u.ϕ)/(1.0 + 1.0/u.ϕ), -1e10)
+    ifelse(l > 1e-10,
+           -u.ξ * l^(1.0 + 1.0/u.ϕ)/(1.0 + 1.0/u.ϕ),
+           -u.ξ * (1e-10^(1.0 + 1.0/u.ϕ)/(1.0 + 1.0/u.ϕ) + 1e-10^(1.0/u.ϕ) * (l - 1e-10)))
 derivative(u::CFEUtility, l::Float64) =
-    ifelse(l > 1e-10, -u.ξ * l^(1.0/u.ϕ), 1e10)
+    ifelse(l > 1e-10, -u.ξ * l^(1.0/u.ϕ), -u.ξ * 1e-10^(1.0/u.ϕ))
 
 
+"""
+Type used to evaluate elliptical utility function. Elliptical utility takes form
+
+v(l) = b (1 - l^μ)^(1 / μ)
+"""
 struct EllipticalUtility <: AbstractUtility
     b::Float64
     μ::Float64
