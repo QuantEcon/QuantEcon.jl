7fb451e253b6fdeb63fc97f4d7d58a3cf88cba2b
Shunsuke Hori <hori0518@gmail.com>
2018-07-10 11:51:52 -0700

    fix logL

--- src/kalman.jl
+++ src/kalman.jl
@@ -155,7 +155,7 @@ function smooth(kn::Kalman, y::AbstractMatrix)
         x_filtered[:, t], sigma_filtered[:, :, t] = kn.cur_x_hat, kn.cur_sigma
         filtered_to_forecast!(kn)
         sigma_forecast[:, :, t] = kn.cur_sigma
-        logL = logL - (log(2*pi) - log(norm(inv(ETA))) + eta'/ETA*eta)[1]
+        logL = logL - (log(2*pi) + logdet(ETA) + eta'/ETA*eta)[1]
     end
     # smoothing
     x_smoothed = copy(x_filtered)
